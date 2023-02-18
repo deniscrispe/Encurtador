cb29d1e26ffcbbab54741497fab285500c29a9a2
Denis <deniscrispe@gmail.com>
2023-02-18 11:57:35 -0300

    feat: Adicionando swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { UrlModule } from './url/url.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/encurtador/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Encurtador de URL')
+    .setDescription('API de encurtador de URL em Nest para fins de estudo')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config, {
+    include: [UrlModule], 
+  });
+  SwaggerModule.setup('/api/encurtador/v1/swagger', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- backend/src/url/url.controller.ts
+++ backend/src/url/url.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Url } from './entities/url.entity';
 import { UrlService } from './url.service';
 import { CreateUrlDto } from './dto/create-url.dto';
 import { UpdateUrlDto } from './dto/update-url.dto';
+import { ApiOkResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Url')
 @Controller('url')
 export class UrlController {
   constructor(private readonly urlService: UrlService) {}
@@ -13,20 +16,45 @@ export class UrlController {
   }
 
   @Get()
+  @ApiOperation({ summary: 'Endpoint para listar todas as URLs cadastradas' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'Lista com JSONs de todas URLs cadastradas', 
+    type: Url,
+    isArray: true 
+  })
   findAll() {
     return this.urlService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Endpoint para buscar URL por id' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'JSON com informações da URL encontrada', 
+    type: Url 
+  })
   findOne(@Param('id') id: string) {
     return this.urlService.findOne(+id);
   }
 
+  @ApiOperation({ summary: 'Endpoint para atualizar URL por id' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'JSON com informações da URL atualizada', 
+    type: Url 
+  })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateUrlDto: UpdateUrlDto) {
     return this.urlService.update(+id, updateUrlDto);
   }
 
+  @ApiOperation({ summary: 'Endpoint para deletar URL por id' })
+  @ApiResponse({ 
+    status: 200, 
+    description: 'Ok', 
+    type: Url 
+  })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.urlService.remove(+id);
